89218e1fc675095044a4ca28163f1b15319e52b7
z0s3r77 <z0s3r77@gmail.com>
2024-02-21 21:51:25 +0100

    Añadido web.xml, servletPrueba, index.jsp

---
+++ src/main/java/prueba/ServeletPrueba.java
@@ -0,0 +1,38 @@
+package prueba;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServeletPrueba
+ */
+@WebServlet(description = "Este servelet es el primero de prueba para configurar el entorno de desarrollo", urlPatterns = { "/ServeletPrueba" })
+public class ServeletPrueba extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public ServeletPrueba() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Servlet de Prueba para configurar el entorno de desarrollo").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
