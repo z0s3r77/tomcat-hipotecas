f974fe308fa96056d1506254f60db4d25696c327
z0s3r77 <z0s3r77@gmail.com>
2024-02-23 17:19:44 +0100

    Añadido entorno testing

---
+++ src/test/java/prueba/ServeletPruebaTest.java
@@ -0,0 +1,16 @@
+package prueba;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ServeletPruebaTest {
+	
+	
+	@Test
+	public void test() {
+		
+		assertTrue(true);
+	}
+
+}
